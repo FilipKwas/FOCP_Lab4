804d3f726612f529f22222aa3217b4dec87bb33b
FilipKwas <filikwa678@student.polsl.pl>
2020-10-31 09:56:58 +0100

    third commit

--- project4/project4/Źródło.cpp
+++ project4/project4/Źródło.cpp
@@ -2,6 +2,23 @@
 
 int main() 
 {
+	int variable = 8;
+
+	int* ptr;
+	
+	std::cout << ptr << std::endl;
+	
+	std::cout << "Memory address of variable: " << &variable << std::endl;
+	std::cout << "Memory address of ptr: " << &ptr << std::endl;
+	
+	ptr = &variable;
+	
+	std::cout << *ptr << std::endl;
+
+	*ptr = 25; 
+
+	std::cout << *ptr << std::endl;
+
 
 	return 0;
 }
